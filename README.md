36b731a651a7a00417ef058e0a63813cca46178d
atproj <andreyltkachuk@gmail.com>
2023-01-20 12:53:10 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# vidometer-navigation-demo
---
+++ webpack.dev.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const mode = 'development';
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode,
+  resolve: {
+    extensions: [".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: path.resolve(__dirname, './public/index.html')
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/assets"),
+          to: path.resolve(__dirname, "./public/assets")
+        },
+      ]
+    }),
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    open: false,
+    port: 8080,
+    https: true,
+  },
+};
